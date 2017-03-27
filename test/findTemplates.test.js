6962069ad7aa86ac2632e49f7eea931b0f5d141e
martypdx <marty@martypdx.com>
2017-03-26 17:45:01 -0700

    more destructure clean up

--- test/findTemplates.test.js
+++ test/findTemplates.test.js
@@ -1,4 +1,3 @@
-/* global describe, it */
 import findTemplates from '../src/findTemplates';
 import parse from '../src/ast';
 import chai from 'chai';
@@ -19,9 +18,8 @@ describe('find templates', () => {
 				const template = (foo, bar) => $\`<span class-foo=\${foo}>hello \${place}</span>\`;
 			`);
 
-
 			assert.equal(templates.length, 1);
-			const { html, bindings, scope, node } = templates[0];
+			const [{ html, bindings, scope, node }] = templates;
 			assert.ok(html);
 			assert.ok(scope);
 			assert.ok(bindings);
@@ -66,7 +64,7 @@ describe('find templates', () => {
 				const template = foo => $\`\*\${foo}\`;
 			`)
 
-			const { html, bindings, scope, node } = templates[0];
+			const [{ html, bindings, scope, node }] = templates;
 			assert.equal(html, '<text-node></text-node>');
 			assert.equal(node.type, 'TaggedTemplateExpression');
 			
@@ -97,34 +95,71 @@ describe('find templates', () => {
 			}]);
 		});
 
-		it('block value', () => {
-			
-			const templates = getTemplates(`
-				const template = place => $\`<div>#\${$\`<span>*\${foo}</span>\`}</div>\`;
-			`)
+		describe('blocks', () => {
 
-			const { html, bindings, scope, node } = templates[0];
-			
-			assert.equal(html, '<div data-bind><section-node></section-node></div>');
-			
-			delete bindings[0].template.node;
+			it('block value', () => {
+				
+				const templates = getTemplates(`
+					const template = place => $\`<div>#\${$\`<span>*\${place}</span>\`}</div>\`;
+				`)
 
-			assert.deepEqual(bindings, [{
-				elIndex: 0,
-				index: 0,
-				type: 'section',
-				template: {
-					html: '<span data-bind><text-node></text-node></span>',
-					bindings: [{
-						elIndex: 0,
-						index: 0,
-						observable: true,
-						ref: 'foo',
-						type: 'child-text'
-					}],
-					scope: void 0
-				}
-			}]);
+				const [{ html, bindings, scope, node }] = templates;
+				
+				assert.equal(html, '<div data-bind><section-node></section-node></div>');
+				
+				// remove node prop for easier deepEqual test 
+				delete bindings[0].template.node;
+
+				assert.deepEqual(bindings, [{
+					elIndex: 0,
+					index: 0,
+					type: 'section',
+					template: {
+						html: '<span data-bind><text-node></text-node></span>',
+						bindings: [{
+							elIndex: 0,
+							index: 0,
+							observable: true,
+							ref: 'place',
+							type: 'child-text'
+						}],
+						scope: void 0
+					}
+				}]);
+			});
+
+			it('conditional block value', () => {
+				
+				const templates = getTemplates(`
+					const template = place => $\`<div>#\${ place && $\`<span>*\${place}</span>\`}</div>\`;
+				`)
+
+				const [{ html, bindings, scope, node }] = templates;
+				
+				assert.equal(html, '<div data-bind><section-node></section-node></div>');
+				
+				// remove node prop for easier deepEqual test 
+				delete bindings[0].template.node;
+
+				assert.deepEqual(bindings, [{
+					elIndex: 0,
+					expr: 'place && $`<span>*${place}</span>`',
+					index: 0,
+					params: ['place'],
+					type: 'section',
+					template: {
+						html: '<span data-bind><text-node></text-node></span>',
+						bindings: [{
+							elIndex: 0,
+							index: 0,
+							observable: true,
+							ref: 'place',
+							type: 'child-text'
+						}],
+						scope: void 0
+					}
+				}]);
+			});
 		});
 	})
 });
