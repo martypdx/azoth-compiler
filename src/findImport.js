3a32f086b8a933e92f54b77af295106df243960e
martypdx <marty@martypdx.com>
2017-03-26 17:44:28 -0700

    use destructring to clean code

--- src/findImport.js
+++ src/findImport.js
@@ -10,7 +10,7 @@ export default function findImport(ast) {
 			// TODO: expose as config so we don't have this weakish test
 			// if(node.source.value !== MODULE_NAME) return;
 			if(!node.source.value.endsWith(MODULE_NAME)) return;
-			specifier = node.specifiers.find(s => s.imported.name === SPECIFIER_NAME);
+			specifier = node.specifiers.find(({ imported }) => imported.name === SPECIFIER_NAME);
 		}
 	});
 	return specifier;
