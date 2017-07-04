c903769439745e42c9faf6c6288a78bf95947699
martypdx <marty@martypdx.com>
2017-07-04 10:33:23 -0700

    allow _ as well as html for template import specifier

--- src/state/imports.js
+++ src/state/imports.js
@@ -1,7 +1,7 @@
 import { RENDERER_IMPORT, MAKE_FRAGMENT_IMPORT } from '../transformers/identifiers';
 import { specifier } from '../transformers/common';
 
-const SPECIFIER_NAME = 'html';
+const SPECIFIER_NAME = /html|_/;
 const baseNames = [RENDERER_IMPORT, MAKE_FRAGMENT_IMPORT];
 const baseSpecifiers = baseNames.map(specifier);
 
@@ -26,7 +26,7 @@ export class Imports {
 
     set specifiers(specifiers) {
         this.ast = specifiers;
-        const index = specifiers.findIndex(({ imported }) => imported.name === SPECIFIER_NAME); 
+        const index = specifiers.findIndex(({ imported }) => SPECIFIER_NAME.test(imported.name)); 
         if(index > -1) {
             this.tag = specifiers[index].local.name;
             specifiers.splice(index, 1);
