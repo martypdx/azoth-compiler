dbafc5782d3e8e359835e56fd876706ad279fa25
martypdx <marty@martypdx.com>
2017-07-09 16:38:59 -0700

    failing tests

--- tests/transformers/binding.test.js
+++ tests/transformers/binding.test.js
@@ -12,12 +12,35 @@ describe('transform - binding', () => {
     it('value', () => {
         const binder = getBinder(
             { ast: (() => foo).toExpr(), sigil: NONE },
-            { module: 1, element: 2 }
+            { module: 1, element: 1 }
         );
         const ast = binding(binder, 0);
         const code = generate(ast);
-        assert.equal(code, '__bind1(__nodes[2])(foo);');
+        assert.equal(code, '__bind1(__nodes[1])(foo);');
     });
+
+    it('no map with NONE', () => {
+        const binder = getBinder(
+            { ast: (() => x + 1).toExpr(), sigil: NONE },
+            { module: 1, element: 2 }
+        );
+        binder.observables = ['x', 'y'];
+        const ast = binding(binder, 1);
+        const code = generate(ast);
+        assert.equal(code, '__bind1(__nodes[2])(x + 1);');
+    });
+
+    it('no combine with NONE', () => {
+        const binder = getBinder(
+            { ast: (() => x + y).toExpr(), sigil: NONE },
+            { module: 1, element: 3 }
+        );
+        binder.observables = ['x', 'y'];
+        const ast = binding(binder, 1);
+        const code = generate(ast);
+        assert.equal(code, '__bind1(__nodes[3])(x + y);');
+    });
+
 
     it('subscribe', () => {
         const binder = getBinder(
@@ -28,6 +51,39 @@ describe('transform - binding', () => {
         const ast = binding(binder, 1);
         const code = generate(ast);
         assert.equal(code, 'const __sub1 = foo.subscribe(__bind1(__nodes[3]));');
+    });
+
+    it('first value', () => {
+        const binder = getBinder(
+            { ast: (() => x).toExpr(), sigil: DOLLAR },
+            { module: 1, element: 3 }
+        );
+        binder.observables = ['x'];
+        const ast = binding(binder, 1);
+        const code = generate(ast);
+        assert.equal(code, 'const __sub1 = __first(x, __bind1(__nodes[3]));');
+    });
+
+    it('first map', () => {
+        const binder = getBinder(
+            { ast: (() => x + 1).toExpr(), sigil: NONE },
+            { module: 1, element: 3 }
+        );
+        binder.observables = ['x'];
+        const ast = binding(binder, 1);
+        const code = generate(ast);
+        assert.equal(code, 'const __sub1 = __map(x, x => x + 1, __bind1(__nodes[3]), true);');
+    });
+
+    it('first combine', () => {
+        const binder = getBinder(
+            { ast: (() => x + y).toExpr(), sigil: NONE },
+            { module: 1, element: 3 }
+        );
+        binder.observables = ['x', 'y'];
+        const ast = binding(binder, 1);
+        const code = generate(ast);
+        assert.equal(code, 'const __sub1 = __combine([x, y], (x, y) => x + y, __bind1(__nodes[3]), true);');
     });
 
     it('map', () => {
@@ -52,37 +108,6 @@ describe('transform - binding', () => {
         assert.equal(code, 'const __sub1 = __combine([x, y], (x, y) => x + y, __bind1(__nodes[3]));');
     });
     
-    it('map first', () => {
-        const binder = getBinder(
-            { ast: (() => x + 1).toExpr(), sigil: NONE },
-            { module: 1, element: 3 }
-        );
-        binder.observables = ['x'];
-        const ast = binding(binder, 1);
-        const code = generate(ast);
-        assert.equal(code, 'const __sub1 = __map(x, x => x + 1, __bind1(__nodes[3]), true);');
-    });
 
-    it('combine first', () => {
-        const binder = getBinder(
-            { ast: (() => x + y).toExpr(), sigil: NONE },
-            { module: 1, element: 3 }
-        );
-        binder.observables = ['x', 'y'];
-        const ast = binding(binder, 1);
-        const code = generate(ast);
-        assert.equal(code, 'const __sub1 = __combine([x, y], (x, y) => x + y, __bind1(__nodes[3]), true);');
-    });
-
-    it('first value', () => {
-        const binder = getBinder(
-            { ast: (() => x).toExpr(), sigil: NONE },
-            { module: 1, element: 3 }
-        );
-        binder.observables = ['x'];
-        const ast = binding(binder, 1);
-        const code = generate(ast);
-        assert.equal(code, 'const __sub1 = __first(x, __bind1(__nodes[3]));');
-    });
     
 });
