8193c752e1a39fe4404eb81da3e2decb1f359ff3
martypdx <marty@martypdx.com>
2017-09-25 16:21:10 -0700

    inital babel setup

---
+++ tests/compilers/compile.test.ava.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import compile from '../../src/compilers/compile.js';
+
+test('hello world', t => {
+    const source = `
+        import { html as _ } from 'azoth';
+        const template = (greeting, name) => _\`<span>\${greeting} \${name}</span>\`;
+    `;
+
+    t.snapshot(compile(source));
+}); 
